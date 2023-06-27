86649c1796f6d8efc85b690cf238287e6206dccc
Александр Абраамян <samvelcc25@outlook.com>
2023-06-27 23:43:35 +0300

    new: token, v and init were added

--- src/ApiRequests/ApiRequests.cpp
+++ src/ApiRequests/ApiRequests.cpp
@@ -2,7 +2,17 @@
 
 namespace vk
 {
-  void sendRequest(const std::string &method, const cpr::Parameters& params)
+  const std::string Requests::baseUrl = "https://api.vk.com/method/";
+  std::string Requests::token = "";
+  std::string Requests::v = "";
+
+  void Requests::init(const std::string &token_, const std::string &v_)
+  { 
+    token = token_;
+    v = v_;
+  }
+
+  cpr::Response Requests::sendRequest(const std::string &method, cpr::Parameters params)
   {
     auto response = cpr::Get(cpr::Url{ baseUrl + method }, params);
     response.error.code != cpr::ErrorCode::OK;
--- src/ApiRequests/ApiRequests.hpp
+++ src/ApiRequests/ApiRequests.hpp
@@ -6,10 +6,21 @@
 
 namespace vk
 {
-  const std::string baseUrl = "https://api.vk.com/method/";
   //TODO pause between requests
   //TODO add token and "v" to parameters
-  cpr::Response sendRequest(const std::string &method, cpr::Parameters params);
+
+  class Requests
+  {
+  public:
+    static void init(const std::string &token_, const std::string &v_);
+    static cpr::Response sendRequest(const std::string &method, cpr::Parameters params);
+  
+  private:
+    static const std::string baseUrl;
+    static std::string token;
+    static std::string v;
+  };
+
 }
 
 #endif
