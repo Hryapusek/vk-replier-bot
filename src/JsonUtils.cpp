222246aae74bc53802d6a2f25984a8cff258c066
Александр Абраамян <samvelcc25@outlook.com>
2023-06-30 00:58:23 +0300

    new: stringToJson method

--- src/JsonUtils.cpp
+++ src/JsonUtils.cpp
@@ -1,13 +1,26 @@
 #include "JsonUtils.hpp"
 
-EventType json_utils::parseEventType(std::reference_wrapper<const Json::Value> root)
+namespace json_utils
 {
-  if (!root.get().isMember("type"))
-    return EventType::UNKNOWN;
-  auto type = root.get()["type"].asString();
-  if (type == "message_new")
-    return EventType::MESSAGE_NEW;
-  else if (type == "confirmation")
-    return EventType::CONFIRMATION;
-  return;
+  EventType parseEventType(std::reference_wrapper< const Json::Value > root)
+  {
+    if (!root.get().isMember("type"))
+      return EventType::UNKNOWN;
+    auto type = root.get()["type"].asString();
+    if (type == "message_new")
+      return EventType::MESSAGE_NEW;
+    else if (type == "confirmation")
+      return EventType::CONFIRMATION;
+    return;
+  }
+
+  Json::Value stringToJsonValue(std::string_view s)
+  {
+    Json::Value root;
+    Json::CharReaderBuilder builder;
+    const std::unique_ptr< Json::CharReader > reader(builder.newCharReader());
+    if (!reader->parse(s.data(), s.data() + s.length(), &root, nullptr))
+      throw Json::Exception("");
+    return root;
+  }
 }
