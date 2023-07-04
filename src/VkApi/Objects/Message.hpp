beb5c7915a93fcad0bbbcdd836e51effac7f661c
Александр Абраамян <samvelcc25@outlook.com>
2023-07-04 17:07:05 +0300

    upd: all object fields are optional now

--- src/VkApi/Objects/Message.hpp
+++ src/VkApi/Objects/Message.hpp
@@ -2,6 +2,7 @@
 #define MESSAGE_HPP
 
 #include <string>
+#include <optional>
 #include <jsoncpp/json/json.h>
 
 namespace vk::objects
@@ -10,14 +11,14 @@ namespace vk::objects
   {
     static const int CHAT_START_ID = 2e9;
   public:
-    int id;
+    std::optional<int> id;
     /// @brief Can be user, chat or group. Destination peer_id.
     /// @note peer_id value is from bot perspective.
     /// Same conversation has different peer_id respectively to users.
-    int peer_id;
+    std::optional<int> peer_id;
     /// @brief Can be user or group. But surely not chat.
-    int from_id;
-    std::string text;
+    std::optional<int> from_id;
+    std::optional<std::string> text;
     bool fromDirect() const;
     bool fromChat() const;
     bool fromGroup() const;
