58ef01b5a413c9c01bdd860aca3af28644b0cdbe
Александр Абраамян <samvelcc25@outlook.com>
2023-07-04 23:15:04 +0300

    refactor: const was added to map value

--- src/MessageProcessing/MessageProcessing.hpp
+++ src/MessageProcessing/MessageProcessing.hpp
@@ -16,9 +16,9 @@ namespace commands
     NONE
   };
 
-  const std::map<Tag, std::vector<std::string>> tagStrings = {
-    {Tag::ALL,           {"_всем_"}},
-    {Tag::ALL_IMPORTANT, {"_всем_важно"}}
+  const std::map< Tag, const std::vector< std::string > > tagStrings = {
+    { Tag::ALL,           { "_всем_" } },
+    { Tag::ALL_IMPORTANT, { "_всем_важно" } }
   };
 
   enum class Command
@@ -34,15 +34,15 @@ namespace commands
     NONE
   };
 
-  const std::map<Command, std::vector<std::string>> commandStrings = {
-    {Command::REG_TARGET,          {"/reg_target"}},
-    {Command::REG_SOURCE,          {"/reg_source"}},
-    {Command::REG_CHECKER,         {"/reg_checker"}},
-    {Command::DEL_TARGET,          {"/del_target"}},
-    {Command::DEL_TARGET_BY_NUM,   {"/del_target_by_num"}},
-    {Command::DEL_SOURCE,          {"/del_source"}},
-    {Command::DEL_CHECKER,         {"/del_checker"}},
-    {Command::PRINT_MAP,           {"/print_map"}},
+  const std::map< Command, const std::vector< std::string > > commandStrings = {
+    { Command::REG_TARGET,          { "/reg_target" } },
+    { Command::REG_SOURCE,          { "/reg_source" } },
+    { Command::REG_CHECKER,         { "/reg_checker" } },
+    { Command::DEL_TARGET,          { "/del_target" } },
+    { Command::DEL_TARGET_BY_NUM,   { "/del_target_by_num" } },
+    { Command::DEL_SOURCE,          { "/del_source" } },
+    { Command::DEL_CHECKER,         { "/del_checker" } },
+    { Command::PRINT_MAP,           { "/print_map" } },
   };
 
   void processMessage(std::shared_ptr< vk::callback::event::objects::NewMessage >);
