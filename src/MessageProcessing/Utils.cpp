39b3eb0c42b844438bac92f027e7ac05c6418194
Александр Абраамян <samvelcc25@outlook.com>
2023-07-09 00:43:29 +0300

    refactor: checking system was upgraded

--- src/MessageProcessing/Utils.cpp
+++ src/MessageProcessing/Utils.cpp
@@ -65,44 +65,35 @@ namespace message_processing::utils
       return extractString(beg, text);
   }
 
-  void logAndSendErrorMessage(MessagesSendRequest &req, str_cref command, str_cref errorMessage)
+  void logAndSendErrorMessage(MessagesSendRequest &req, str_cref commandName, str_cref errorMessage)
   {
-    BOOST_LOG_TRIVIAL(warning) << command << ": " << errorMessage << ". Skipping message";
+    BOOST_LOG_TRIVIAL(warning) << commandName << ": " << errorMessage << ". Skipping message";
     req.message(errorMessage).execute();
   }
 
-  bool checkIfCommandFromChat(const Message &message, str_cref command)
+  void sendMessageToAllTargets(str_cref text, int fwd_msg_id)
   {
-    if (!message.fromChat())
-    {
-      BOOST_LOG_TRIVIAL(warning) << "Command  " << command << " not from chat. Skipping message";
-      return false;
-    }
-    return true;
+    MessagesSendRequest()
+    .peer_ids(ConfigHolder::getTargetIds())
+    .random_id(0)
+    .forward_messages(std::to_string(fwd_msg_id))
+    .message(text)
+    .execute();
   }
 
-  bool checkIfChatIsSource(const Message &message, MessagesSendRequest &req, str_cref command, str_cref errorMessage)
+  bool checkIfCommandFromChat(const Message &message, str_cref commandName, str_cref errorMessage)
+  {
+    return checkIf(message.fromChat(), commandName, errorMessage);
+  }
+
+  bool checkIfChatIsNotSource(int peerId, MessagesSendRequest &req, str_cref commandName, str_cref errorMessage)
   {
     auto sourceChat = ConfigHolder::getReadOnlyConfig().config.sourceChat;
-    if (sourceChat && sourceChat.value().peer_id == message.getPeerId())
-    {
-      logAndSendErrorMessage(req, command, errorMessage);
-      return false;
-    }
-    return true;
+    bool cond = sourceChat && sourceChat->peer_id == peerId;
+    return checkIf(cond, req, commandName, errorMessage);
   }
 
-  bool checkIfChatPresentInTable(const Message &message, MessagesSendRequest &req, str_cref command, str_cref errorMessage)
-  {
-    if (ConfigHolder::getReadOnlyConfig().config.targetsTable.containsPeerId(message.getPeerId()))
-    {
-      logAndSendErrorMessage(req, command, errorMessage);
-      return false;
-    }
-    return true;
-  }
-
-  bool checkMode(Mode mode, str_cref errorMessage)
+  bool checkMode(Mode mode, str_cref commandName, str_cref errorMessage)
   {
     if (ConfigHolder::getMode() != mode)
     {
@@ -116,41 +107,85 @@ namespace message_processing::utils
         case Mode::WORK:
           postfix = " - mode is not WORK";
           break;
+        
+        default:
+          postfix = "- CAN NOT CHECK THE MODE. IT'S NOT INITIALIZED!!!";
       }
-      BOOST_LOG_TRIVIAL(warning) << errorMessage << postfix;
+      BOOST_LOG_TRIVIAL(warning) << commandName << ": " << errorMessage << postfix;
       return false;
     }
     return true;
   }
 
-  bool checkIfSourceChatPresent(str_cref command, str_cref errorMessage)
-  {
-    if (!config::ConfigHolder::getReadOnlyConfig().config.sourceChat)
-    {
-      BOOST_LOG_TRIVIAL(warning) << command << ": " << errorMessage;
-      return false;
-    }
-    return true;
+  bool checkIfSourceChatPresent(str_cref commandName, str_cref errorMessage)
+  {    
+    bool cond = config::ConfigHolder::getReadOnlyConfig().config.sourceChat.has_value();
+    return checkIf(cond, commandName, errorMessage);
   }
 
-  void sendMessageToAllTargets(str_cref text, int fwd_msg_id)
-  {
-    MessagesSendRequest req;
-    req.peer_ids(config::ConfigHolder::getTargetIds())
-    .random_id(0)
-    .message(text)
-    .forward_messages(std::to_string(fwd_msg_id))
-    .execute();
-  }
-
-  bool checkIfChatIsSource(int peerId, str_cref command, str_cref errorMessage)
+  bool checkIfChatIsNotSource(int peerId, str_cref commandName, str_cref errorMessage)
   {
     auto sourceChat = config::ConfigHolder::getReadOnlyConfig().config.sourceChat;
-    if (sourceChat && sourceChat.value().peer_id == peerId)
-    {
-      BOOST_LOG_TRIVIAL(warning) << command << ": " << errorMessage;
-      return false;
-    }
-    return true;
+    bool cond = !(sourceChat && sourceChat->peer_id == peerId);
+    return checkIf(cond, commandName, errorMessage);
+  }
+
+  bool checkIfChatIsSource(int peerId, str_cref commandName, str_cref errorMessage)
+  {
+    auto sourceChat = config::ConfigHolder::getReadOnlyConfig().config.sourceChat;
+    bool cond = sourceChat && sourceChat->peer_id == peerId;
+    return checkIf(cond, commandName, errorMessage);
+  }
+
+  bool checkIfChatIsSource(int peerId, vk::requests::messages::MessagesSendRequest & req, str_cref commandName, str_cref errorMessage)
+  {
+    auto sourceChat = config::ConfigHolder::getReadOnlyConfig().config.sourceChat;
+    bool cond = sourceChat && sourceChat->peer_id == peerId;
+    return checkIf(cond, req, commandName, errorMessage);
+  }
+
+  bool checkIfPeerIdInTargetsTable(const Message &message, MessagesSendRequest &req, str_cref commandName, str_cref errorMessage)
+  {
+    bool cond = config::ConfigHolder::getReadOnlyConfig().config.targetsTable.containsPeerId(message.getPeerId());
+    return checkIf(cond, req, commandName, errorMessage);
+  }
+
+  bool checkIfPeerIdNotInTargetsTable(const Message &message, MessagesSendRequest &req, str_cref commandName, str_cref errorMessage)
+  {
+    bool cond = !config::ConfigHolder::getReadOnlyConfig().config.targetsTable.containsPeerId(message.getPeerId());
+    return checkIf(cond, req, commandName, errorMessage);
+  }
+
+  bool checkIfFromDirect(const Message & message, str_cref commandName, str_cref errorMessage)
+  {
+    return checkIf(message.fromDirect(), commandName, errorMessage);
+  }
+
+  bool checkIfChecker(const Message &message, MessagesSendRequest &req, str_cref commandName, str_cref errorMessage)
+  {
+    const auto &checkers = config::ConfigHolder::getReadOnlyConfig().config.statusCheckersIds;
+    bool cond = std::find(checkers.cbegin(), checkers.cend(), message.getPeerId()) != checkers.end();
+    return checkIf(cond, req, commandName, errorMessage);
+  }
+
+  bool checkIfNotChecker(const Message &message, str_cref commandName, str_cref errorMessage)
+  {
+    const auto &checkers = config::ConfigHolder::getReadOnlyConfig().config.statusCheckersIds;
+    bool cond = std::find(checkers.cbegin(), checkers.cend(), message.getPeerId()) == checkers.end();
+    return checkIf(cond, commandName, errorMessage);
+  }
+
+  bool checkIf(bool cond, MessagesSendRequest & req, str_cref commandName, str_cref errorMessage)
+  {
+    if (!cond)
+      logAndSendErrorMessage(req, commandName, errorMessage);
+    return cond;
+  }
+
+  bool checkIf(bool cond, str_cref commandName, str_cref errorMessage)
+  {
+    if (!cond)
+      BOOST_LOG_TRIVIAL(warning) << commandName << ": " << errorMessage;
+    return cond;
   }
 }
--- src/MessageProcessing/Utils.hpp
+++ src/MessageProcessing/Utils.hpp
@@ -26,16 +26,48 @@ namespace message_processing::utils
   /// Quoted title otherwise.
   /// @throw \b std::exception if unclosed quote found
   std::string extractTitle(str_cref text, size_t pos);
-  void logAndSendErrorMessage(vk::requests::messages::MessagesSendRequest &req, str_cref command, str_cref errorMessage);
-  bool checkIfCommandFromChat(const vk::objects::Message &message, str_cref command);
-  bool checkIfChatIsSource(const vk::objects::Message &message, vk::requests::messages::MessagesSendRequest &req,
-                            str_cref command, str_cref errorMessage);
-  bool checkIfChatIsSource(int peerId, str_cref command, str_cref errorMessage);
-  bool checkIfChatPresentInTable(const vk::objects::Message &message, vk::requests::messages::MessagesSendRequest &req,
-                                  str_cref command, str_cref errorMessage);
-  bool checkMode(config::Mode mode, str_cref errorMessage);
-  bool checkIfSourceChatPresent(str_cref command, str_cref errorMessage);
+  void logAndSendErrorMessage(vk::requests::messages::MessagesSendRequest &req, str_cref commandName, str_cref errorMessage);
+
   void sendMessageToAllTargets(str_cref text, int fwd_msg_id);
+
+  bool checkIfCommandFromChat(const vk::objects::Message &message, str_cref commandName, str_cref errorMessage);
+
+  bool checkIfChatIsNotSource(int peerId, vk::requests::messages::MessagesSendRequest &req,
+                              str_cref commandName, str_cref errorMessage);
+
+  bool checkIfChatIsNotSource(int peerId, str_cref commandName, str_cref errorMessage);
+
+  bool checkIfChatIsSource(int peerId, str_cref commandName, str_cref errorMessage);
+
+  bool checkIfChatIsSource(int peerId, vk::requests::messages::MessagesSendRequest &req,
+                           str_cref commandName, str_cref errorMessage);
+
+  bool checkMode(config::Mode mode, str_cref commandName, str_cref errorMessage);
+
+  bool checkIfSourceChatPresent(str_cref commandName, str_cref errorMessage);
+
+  bool checkIfPeerIdInTargetsTable(const vk::objects::Message &message, vk::requests::messages::MessagesSendRequest &req,
+                                   str_cref commandName, str_cref errorMessage);
+
+  bool checkIfPeerIdNotInTargetsTable(const vk::objects::Message &message, vk::requests::messages::MessagesSendRequest &req,
+                                      str_cref commandName, str_cref errorMessage);
+
+  bool checkIfFromDirect(const vk::objects::Message &message, str_cref commandName, str_cref errorMessage);
+
+  bool checkIfChecker(const vk::objects::Message &message, vk::requests::messages::MessagesSendRequest &req,
+                             str_cref commandName, str_cref errorMessage);
+
+  bool checkIfNotChecker(const vk::objects::Message &message, str_cref commandName, str_cref errorMessage);
+
+  /// @brief if cond is False - logAndSendErrorMessage called.
+  /// If True - nothing happens.
+  /// @return cond
+  bool checkIf(bool cond, vk::requests::messages::MessagesSendRequest &req, str_cref commandName, str_cref errorMessage);
+
+  /// @brief if cond is False - errorMessage will be logged.
+  /// If True - nothing happens.
+  /// @return cond
+  bool checkIf(bool cond, str_cref commandName, str_cref errorMessage);
 }
 
 #endif
