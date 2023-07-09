5c4dc7654ae26cf32866bd7a40093e1f0211d0df
Александр Абраамян <samvelcc25@outlook.com>
2023-07-09 19:54:44 +0300

    upd: testing system was upgraded

---
+++ test/jsons/registration/trace1/info.md
@@ -0,0 +1,26 @@
+peer_ids:
+  1.  2000000001 - **Source** chat with **bad title**
+  2.  2000000001 - **Source** chat with **bad title**
+  3.  2000000001 - **Source** chat with **title**
+  4.  /del_source
+  5.  2000000001 - **Source** chat without **title**
+  
+  6.  2000000100 - **Target** chat with **num** and **bad title**
+  7.  2000000100 - **Target** chat with **bad num** and **title**
+  8.  2000000100 - **Target** chat with **bad title**
+  9.  2000000100 - **Target** chat with **bad num**
+  10. 2000000100 - **Target** chat with **title**
+  11. /del_target
+  12. 2000000100 - **Target** chat with **num**
+  13. /del_target
+  14. 2000000100 - **Target** chat with **num** and **title**
+  15. 2000000101 - **Target** chat with same **num** to check if error
+  16. 2000000101 - **Target** chat
+  17. 2000000102 - **Target** chat
+ 18.1 2000000103 - **Target** chat
+ 18.2 2000000103 - **Target** chat to check if error
+
+  19. 2000000050 - **Status** checker
+  20. 2000000051 - **Status** checker
+  21. 2000000052 - **Status** checker
+  22. 2000000052 - **Status** checker to check if error
--- test/test.py
+++ test/test.py
@@ -7,40 +7,61 @@ import sys
 
 botUrl = "http://localhost:8081/notification"
 port = 8080
+sep = "-"*25
+
 
 class Unit:
     def __init__(self, fileName: str, description: str) -> None:
         self.fileName = fileName
         self.description = description
 
+
 serverThread = threading.Thread(target=server.run, args=[port], daemon=True)
 serverThread.start()
 
-units = [
-    Unit("jsons/message_new/good/1.json", "No command message"),
-    Unit("jsons/message_new/good/2.json", "No command message"),
-    Unit("jsons/message_new/good/3.json", "No command message"),
-    Unit("jsons/message_new/good/4.json", "No command message"),
-    Unit("jsons/message_new/good/5.json", "No command message"),
-]
-
-if len(sys.argv) == 1:
-    for unit in units:
-        f = open(unit.fileName)
+def executeUnit(fileName: str):
+    with open(fileName) as f: 
         content = f.read()
-        f.close()
-        print("File:", unit.fileName)
-        print("Description:", unit.description)
+        print("File:", fileName)
         print("Content:", content)
         response = requests.post(url=botUrl, json=json.loads(content))
         print(response.status_code)
-        time.sleep(0.7)
+        # time.sleep(0.4)
+        print(sep)
+
+if len(sys.argv) <= 1:
+    print("Wronk usage")
+
+elif sys.argv[1] == "registration/trace1":
+    units = [
+        Unit("jsons/registration/trace1/1.json", ""),
+        Unit("jsons/registration/trace1/2.json", ""),
+        Unit("jsons/registration/trace1/3.json", ""),
+        Unit("jsons/registration/trace1/4.json", ""),
+        Unit("jsons/registration/trace1/5.json", ""),
+        Unit("jsons/registration/trace1/6.json", ""),
+        Unit("jsons/registration/trace1/7.json", ""),
+        Unit("jsons/registration/trace1/8.json", ""),
+        Unit("jsons/registration/trace1/9.json", ""),
+        Unit("jsons/registration/trace1/10.json", ""),
+        Unit("jsons/registration/trace1/11.json", ""),
+        Unit("jsons/registration/trace1/12.json", ""),
+        Unit("jsons/registration/trace1/13.json", ""),
+        Unit("jsons/registration/trace1/14.json", ""),
+        Unit("jsons/registration/trace1/15.json", ""),
+        Unit("jsons/registration/trace1/16.json", ""),
+        Unit("jsons/registration/trace1/17.json", ""),
+        Unit("jsons/registration/trace1/18.1.json", ""),
+        Unit("jsons/registration/trace1/18.2.json", ""),
+        Unit("jsons/registration/trace1/19.json", ""),
+        Unit("jsons/registration/trace1/20.json", ""),
+        Unit("jsons/registration/trace1/21.json", ""),
+        Unit("jsons/registration/trace1/22.json", ""),
+    ]
+    for unit in units:
+        executeUnit(unit.fileName)
+
 else:
-    f = open(sys.argv[1])
-    content = f.read()
-    f.close()
-    print("File:", sys.argv[1])
-    print("Content:", content)
-    response = requests.post(url=botUrl, json=json.loads(content))
-    print(response.status_code)
-    time.sleep(0.7)
+    executeUnit(sys.argv[1])
+
+time.sleep(1000)
