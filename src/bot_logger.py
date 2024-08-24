358477105675c9b9241ea9104830e63ac449eec0
Abraamyan Alexander <hryapusek@gmail.com>
2024-08-24 22:53:43 +0300

    logger rotation added

--- src/bot_logger.py
+++ src/bot_logger.py
@@ -1,5 +1,5 @@
 from loguru import logger as _logger
 
-_logger.add("log_file.log", format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", level="DEBUG")
+_logger.add("log_file.log", format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", level="DEBUG", rotation="00:00")
 
 logger = _logger
